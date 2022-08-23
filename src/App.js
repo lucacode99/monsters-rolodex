2093c075f8f6081e46fa415fcbd095e304f85c25
Lucas Pfister <digitalservices@outlook.de>
2022-08-23 14:41:11 +0200

    working on filter input

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-
 class App extends Component {
   constructor () {
     super();
@@ -18,19 +17,31 @@ class App extends Component {
     .then((users) => this.setState(()=> {
       return {monsters: users}
     },
-    () => {console.log(this.state)}
+    () => {}
     ))
   }
 
-
   render() {
-
   return (
     <div className="App">
+    <input 
+      className='search-box' 
+      type='search' 
+      placeholder='search monsters' 
+      onChange={(event) => {
+        console.log(event.target.value)
+        const filteredMonsters = this.state.monsters.filter((monster) => {
+          return monster.name.includes(event.target.value);
+        });
+        }}
+    />
+        
       {this.state.monsters.map((monster) => {
-        return <div key={monster.id}>
+        return (
+        <div key={monster.id}>
         <h1>{monster.name}</h1>
-        </div>       
+        </div>  
+      )     
       })}
     </div>
   );
